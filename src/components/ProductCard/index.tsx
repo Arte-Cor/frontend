9ebd93bd6fc71d0bce46599be6266980706db8f8
wueliton <paulo.wueliton@hotmail.com>
2022-08-15 10:22:16 -0300

    adicionado link do produto

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,40 +1,43 @@
 import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
 import { ProductModel } from 'src/models/dynamic-component.model';
 
 export const ProductCard: React.FC<{ product: ProductModel }> = ({
   product,
 }) => {
   return (
-    <Box
-      sx={{
-        borderRadius: 2,
-        border: '1px solid #EEE',
-        padding: 1,
-        background: 'white',
-        textAlign: 'center',
-      }}
-    >
+    <Link href={product.attributes.slug}>
       <Box
-        component="img"
-        src={product.attributes.url_image}
         sx={{
-          aspectRatio: '1/1',
-          display: 'block',
-          width: '100%',
-          objectFit: 'cover',
-          marginBottom: 2,
+          borderRadius: 2,
+          border: '1px solid #EEE',
+          padding: 1,
+          background: 'white',
+          textAlign: 'center',
         }}
-      ></Box>
-      <Typography variant="body2">{product.attributes.title}</Typography>
-      <Typography
-        variant="button"
-        sx={{ fontSize: 20, fontWeight: 'bold', fontFamily: 'K2D' }}
       >
-        {new Intl.NumberFormat('pt-br', {
-          style: 'currency',
-          currency: 'BRL',
-        }).format(product.attributes.price)}
-      </Typography>
-    </Box>
+        <Box
+          component="img"
+          src={product.attributes.url_image}
+          sx={{
+            aspectRatio: '1/1',
+            display: 'block',
+            width: '100%',
+            objectFit: 'cover',
+            marginBottom: 2,
+          }}
+        ></Box>
+        <Typography variant="body2">{product.attributes.title}</Typography>
+        <Typography
+          variant="button"
+          sx={{ fontSize: 20, fontWeight: 'bold', fontFamily: 'K2D' }}
+        >
+          {new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(product.attributes.price)}
+        </Typography>
+      </Box>
+    </Link>
   );
 };
